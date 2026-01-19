0219bba1a64caab78e198141d373255320b2015f
Fredrik Nilson <fredrik.nilson@datatrion.se>
2026-01-19 08:56:11 +0100

    Höger space blir enter, samt båda blir symbollagret vid håll och fn1 och fn2 går till lager funktionslagret

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/eurkey/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/eurkey/keymap.c
@@ -43,6 +43,8 @@ enum layers{
 #define HM_L    RCTL_T(KC_L) // Control + L
 #define HM_ODIA RSFT_T(EU_ODIA) // Shift + Ö
 
+// -------------------- Tab dance --------------------s
+
 enum tap_dance_codes {
   DANCE_0,
   DANCE_1,
@@ -406,6 +408,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
+
 // -------------------- Key Overrides --------------------
 
 // Shift + , → ;
@@ -437,14 +440,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   EU_ARNG,  KC_QUOT,  KC_SLSH,          KC_DEL,
         MO(FN2), HM_A,     HM_S,     HM_D,    HM_F,    KC_G,              KC_H,    HM_J,    HM_K,    HM_L,     HM_ODIA,  EU_ADIA,  KC_ENT,           KC_HOME,
         KC_LSFT,           TD_Z,	 TD_X,	  TD_C,    TD_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_QUES,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPTN, KC_LCMMD,          KC_SPC,           MO(MAC_FN1), MO(FN2),       KC_SPC,            KC_RCMMD,           KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LOPTN,           KC_LCMMD, LT(MAC_FN1, KC_SPC), MO(FN2), MO(FN2), LT(MAC_FN1, KC_ENT), KC_RCMMD,               KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
         KC_ESC,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   EU_ARNG,  KC_QUOT,  KC_SLSH,          KC_DEL,
         MO(FN2), KC_A,	   KC_S,	 KC_D,	  KC_F,    KC_G,              KC_H,    KC_J,	KC_K,	 KC_L,	   EU_ODIA,  EU_ADIA,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_QUES,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LALT,  KC_LWIN,           KC_SPC,           MO(WIN_FN1), MO(FN2),       KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LALT,            KC_LWIN, LT(MAC_FN1, KC_SPC), MO(FN2), MO(FN2), LT(MAC_FN1, KC_ENT), KC_RALT,                 KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_FN1] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU, KC_MCTRL, KC_LNPAD,RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
