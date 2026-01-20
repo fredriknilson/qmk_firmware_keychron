77d7d263d800eaffc27011c4dea5402f37b6be59
Fredrik Nilson <fredrik.nilson@datatrion.se>
2026-01-20 10:06:50 +0100

    Tar bort layershift på spacebar på windowslagret

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/eurkey/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/eurkey/keymap.c
@@ -447,7 +447,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   EU_ARNG,  KC_QUOT,  KC_SLSH,          KC_DEL,
         MO(FN2), KC_A,	   KC_S,	 KC_D,	  KC_F,    KC_G,              KC_H,    KC_J,	KC_K,	 KC_L,	   EU_ODIA,  EU_ADIA,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_QUES,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LALT,            KC_LWIN, LT(MAC_FN1, KC_SPC), MO(FN2), MO(FN2), LT(MAC_FN1, KC_ENT), KC_RALT,                 KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LALT,            KC_LWIN, KC_SPC, MO(FN2), MO(FN2), LT(MAC_FN1, KC_ENT), KC_RALT,                 KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_FN1] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU, KC_MCTRL, KC_LNPAD,RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
